e67a767a0b4d6b8be9875270d4a0cd4441170060
Ed-cred <edwardandrew63@gmail.com>
2023-09-19 12:07:31 +0300

    minHeap impl

--- src/day2/MinHeap.ts
+++ src/day2/MinHeap.ts
@@ -1,15 +1,74 @@
 export default class MinHeap {
     public length: number;
-
-    
+    private data: number[];
 
     constructor() {
+        this.data = [];
+        this.length = 0;
     }
 
     insert(value: number): void {
-
-}
+        this.data[this.length] = value;
+        this.heapifyUp(this.length);
+        this.length++;
+    }
     delete(): number {
+        if (this.length === 0) {
+            return -1;
+        }
+        const out = this.data[0];
+        this.length--;
+        if (this.length === 0) {
+            this.data = [];
+            return out;
+        }
+        this.data[0] = this.data[this.length];
+        this.heapifyDown(0);
+        return out;
+    }
 
-}
+    private heapifyDown(idx: number): void {
+        const lIdx = this.leftChild(idx);
+        const rIdx = this.rightChild(idx);
+        const lVal = this.data[lIdx];
+        const rVal = this.data[rIdx];
+        const v = this.data[idx];
+        if (idx >= this.length || lIdx >= this.length) {
+            return;
+        }
+        if (lVal > rVal && v > rVal) {
+            this.data[idx] = rVal;
+            this.data[rIdx] = v;
+            this.heapifyDown(rIdx);
+        } else if (rVal > lVal && v > lVal) {
+            this.data[idx] = lVal;
+            this.data[lIdx] = v;
+            this.heapifyDown(lIdx);
+        }
+    }
+
+    private heapifyUp(idx: number): void {
+        if (idx === 0) {
+            return;
+        }
+        const p = this.parent(idx);
+        const parentVal = this.data[p];
+        const v = this.data[idx];
+        if (parentVal > v) {
+            this.data[idx] = parentVal;
+            this.data[p] = v;
+            this.heapifyUp(p);
+        }
+    }
+    private parent(idx: number): number {
+        return Math.floor((idx - 1) / 2);
+    }
+
+    private leftChild(idx: number): number {
+        return idx * 2 + 1;
+    }
+
+    private rightChild(idx: number): number {
+        return idx * 2 + 2;
+    }
 }
